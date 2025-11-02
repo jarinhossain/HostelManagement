419578fba558370a5846ae0d4ddc3dc9c925b313
jarin <jarinhossain786@gmail.com>
2025-11-02 22:00:03 +0600

    minor

--- HostelManagement/WeatherForecast.cs
+++ HostelManagement/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace HostelManagement
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureFa => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
